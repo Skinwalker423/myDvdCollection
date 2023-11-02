3401144428f2bd5214a04178d071eb8990d07f25
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-02 16:41:55 -0700

    location

--- src/routes/movie.tsx
+++ src/routes/movie.tsx
@@ -15,6 +15,22 @@ const Movie = () => {
   let location = "";
   if (foundMovie.id < 19) {
     location = `First row, ${foundMovie.id} going in from the left`;
+  } else if (foundMovie.id < 46) {
+    location = `Second row, ${
+      foundMovie.id - 18
+    } going in from the left`;
+  } else if (foundMovie.id < 81) {
+    location = `Thrid row, ${
+      foundMovie.id - 45
+    } going in from the left`;
+  } else if (foundMovie.id < 121) {
+    location = `Fourth row, ${
+      foundMovie.id - 80
+    } going in from the left`;
+  } else if (foundMovie.id < 157) {
+    location = `Second row, ${
+      foundMovie.id - 120
+    } going in from the left`;
   }
   return (
     <main className='flex justify-center gap-5'>
