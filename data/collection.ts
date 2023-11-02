f85d6ecc3517f0cc338df96313b5ffd8ed61f5bf
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-01 18:17:16 -0700

    images

--- data/collection.ts
+++ data/collection.ts
@@ -1,1 +1,68 @@
-const collection = [{}];
+const collection = [
+  {
+    id: 1,
+    title: "Lost Season 1",
+    image: "/images/lost.jpg",
+    year: 2004,
+  },
+  {
+    id: 2,
+    title: "Lost Season 2",
+    image: "/images/lost.jpg",
+    year: 2005,
+  },
+  {
+    id: 3,
+    title: "The incredible HUlk the complete series",
+    image: "/images/hulk.jpg",
+    year: 1977,
+  },
+  {
+    id: 4,
+    title: "Avatar",
+    image: "/images/avatar.jpg",
+    year: 2009,
+  },
+  {
+    id: 5,
+    title: "The Kennedy's",
+    image: "/images/kennedys.jpg",
+    year: 2011,
+  },
+  {
+    id: 6,
+    title: "Lost Season 1",
+    image: "",
+    year: 2004,
+  },
+  {
+    id: 7,
+    title: "Lost Season 1",
+    image: "",
+    year: 2004,
+  },
+  {
+    id: 8,
+    title: "Lost Season 1",
+    image: "",
+    year: 2004,
+  },
+  {
+    id: 9,
+    title: "Lost Season 1",
+    image: "",
+    year: 2004,
+  },
+  {
+    id: 10,
+    title: "Lost Season 1",
+    image: "",
+    year: 2004,
+  },
+  {
+    id: 11,
+    title: "Lost Season 1",
+    image: "",
+    year: 2004,
+  },
+];
