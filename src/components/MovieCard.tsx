81828c7a2fe45b1d2bf283d4689441c73905564b
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-02 17:18:19 -0700

    default img src

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,9 +2,14 @@ import { MovieProps } from "../../types";
 import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }: MovieProps) => {
+  const img =
+    movie.image === ""
+      ? "/images/defaultImg.png"
+      : movie.image;
+
   return (
     <Link to={`/movies/${movie.id}`}>
-      <div className='h-full shadow-lg hover:scale-105 transition-transform'>
+      <div className='h-full hover:scale-105 transition-transform'>
         <div className='py-4 px-2 w-72 bg-slate-400'>
           <h3 className='text-white z-50 text-sm'>
             {movie.title}
@@ -13,7 +18,7 @@ const MovieCard = ({ movie }: MovieProps) => {
         <div className='w-72 h-96'>
           <img
             className='object-cover w-full h-full'
-            src={movie.image}
+            src={img}
             alt={movie.title}
           />
         </div>
