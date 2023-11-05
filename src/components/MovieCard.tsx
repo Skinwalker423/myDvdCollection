577c54694c2f4757bf987534bc8c8a99a6dedb1f
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-04 17:09:41 -0700

    removed img in card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,11 +2,6 @@ import { MovieProps } from "../../types";
 import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }: MovieProps) => {
-  const img =
-    movie.image === ""
-      ? "/images/defaultImg.png"
-      : movie.image;
-
   return (
     <li className='w-full h-full max-w-6xl hover:scale-105 transition-transform rounded-lg py-4 px-2 bg-slate-200'>
       <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
